43c5323193c17610b8d88aa54f1855a0e3aa2d81
Marlon Almino <alminomarlon@gmail.com>
2023-04-10 16:35:18 -0300

    iniciando arquivos de páginas e rotas; utilizando navigator

---
+++ src/pages/home/index.js
@@ -0,0 +1,15 @@
+import { View, Text, StyleSheet } from 'react-native'
+
+export function Home() {
+    return(
+        <View style={styles.container}>
+            <Text>Página Home!!!</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'red'
+    }
+})
