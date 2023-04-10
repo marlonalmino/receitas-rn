51c62828a4e77e8084836d597df81a6f633b84d5
Marlon Almino <alminomarlon@gmail.com>
2023-04-10 18:12:56 -0300

    Utilizando hooks no input

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,17 @@
+import { useState } from 'react'
 import { View, Text, StyleSheet, SafeAreaView, TextInput, TouchableOpacity } from 'react-native'
-
 import { Ionicons } from '@expo/vector-icons'
 
 import { Logo } from '../../components/logo'
 
 export function Home() {
+
+    const [inputValue, setInputValue] = useState('')
+
+    function handleSearch() {
+        console.log(`Você digitou: ${inputValue}`)
+    }
+
     return(
         <SafeAreaView style={styles.container}>
             <Logo/>
@@ -15,8 +22,10 @@ export function Home() {
                 <TextInput
                     placeholder='Digite o nome da comida...'
                     style={styles.input}
+                    value={inputValue}
+                    onChangeText={ (text) => setInputValue(text) }
                 />
-                <TouchableOpacity>
+                <TouchableOpacity onPress={handleSearch}>
                     <Ionicons name='search' size={28} color='#4CBE6C' />
                 </TouchableOpacity>
             </View>
