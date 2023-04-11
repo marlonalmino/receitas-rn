8c1ee054ce96e32673ad062fbae6c792ef79edec
Marlon Almino <alminomarlon@gmail.com>
2023-04-11 16:51:26 -0300

    Iniciando page detail

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,9 +1,36 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { useLayoutEffect } from 'react'
+import { View, Text, StyleSheet, Pressable } from 'react-native'
+import { useRoute, useNavigation } from '@react-navigation/native'
+
+import { Entypo } from '@expo/vector-icons'
 
 export function Detail() {
+    const route = useRoute()
+    const navigation = useNavigation()
+
+    // Destructuring 'route'
+    const { name } = route.params?.data
+
+
+    useLayoutEffect( () => {
+        navigation.setOptions({
+            title: route.params?.data ? name : 'Detalhes da receita',
+            headerRight: () => (
+                <Pressable onPress={ () => console.log('testando')}>
+                    <Entypo
+                        name='heart'
+                        size={28}
+                        color='#FF4141'
+                    />
+                </Pressable>
+            )
+        })
+    }, [navigation, route.params?.data])
+
     return(
         <View style={styles.container}>
             <Text>Página Detail!!!</Text>
+            <Text>{route.params?.data.name}</Text>
         </View>
     )
 }
